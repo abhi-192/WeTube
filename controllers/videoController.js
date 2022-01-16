411dbc9b311658202fac963f0486a3b0bb558466
abhi-192 <contactabhi.2020@gmail.com>
2022-01-16 11:11:45 +0530

    Join login template add

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,8 @@
 const home = (req,res) => res.render("home", { pageTitle: "Home" });
-const search = (req,res) => res.render('search', { pageTitle: "Search" });
+const search = (req,res) => {   
+    const { query: { term: searchingBy } } = req;
+    res.render('search', { pageTitle: "Search" , searchingBy });
+}
 const videos = (req,res) => res.render('video', { pageTitle: "Video" });
 const upload = (req,res) => res.render('upload',  { pageTitle: "Upload" });
 const videoDetail = (req,res) => res.render('videoDetail', { pageTitle: "Video Detail" });
