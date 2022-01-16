96c6677a83f456b419bbed6627e56ebf6e4823c9
abhi-192 <contactabhi.2020@gmail.com>
2022-01-16 17:03:09 +0530

    getJoin and postJoin

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,25 @@
+const routes = require('../routes')
+
 const login = (req,res) => res.render('login', { pageTitle: "Log In" });
 const logout = (req,res) => res.render('logout', { pageTitle: "Log Out" });
-const join = (req,res) => res.render('join', { pageTitle: "Join" });
+
+const getJoin = (req,res) => res.render('join', { pageTitle: "Join" });
+const postJoin = (req,res) => {
+    console.log(req.body);
+    const {
+        body: { name, email, password, password2 }
+    } = req;
+    if( password !== password2 ){
+        res.status(400);
+        res.render('join', { pageTitle: "Join" });
+    }
+    else{
+        // TO DO : Register user
+        // To DO : Login user
+        res.redirect(routes.home);
+    }
+}
+
 const users = (req,res) => res.render('user', { pageTitle: "User" });
 const userDetails = (req,res) => res.render('userDetails', { pageTitle: "User Details" });
 const editProfile = (req,res) => res.render('editProfile', { pageTitle: "Edit Profile" });
@@ -10,7 +29,8 @@ const changePassword = (req,res) => res.render('changePassword', { pageTitle: "C
 module.exports = {
     login,
     logout,
-    join,
+    getJoin,
+    postJoin,
     users,
     userDetails,
     editProfile,
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { logout, login, join } = require('../controllers/usersController');
+ const express = require('express');
+const { logout, login, getJoin, postJoin } = require('../controllers/usersController');
 const { home, search} = require('../controllers/videoController')
 const globalRouter = express.Router();
 const routes = require('../routes');
@@ -7,7 +7,10 @@ const routes = require('../routes');
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
-globalRouter.get(routes.join, join);
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.search, search);
 
 module.exports = globalRouter;
