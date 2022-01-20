fa3e3d58ed841be3680c19fa93b85b88499115e3
abhi-192 <contactabhi.2020@gmail.com>
2022-01-20 10:39:54 +0530

    videoRecorder complete

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,8 +1,22 @@
 const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
+let streamObject;
 
-const startRecording = async () => {
+const handleVideoData = (event) => {
+    console.log(event.data);
+}
+
+const startRecording = () => {
+    const videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start(1000);
+    videoRecorder.addEventListener("dataavailable", handleVideoData);
+    // MediaRecorder  records in one chunk 
+    // so after some time keep on stop recording and start again
+    // so that stream can be made avaiable
+}
+
+const getVideo = async () => {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: true,
@@ -11,14 +25,19 @@ const startRecording = async () => {
         videoPreview.srcObject = stream;
         videoPreview.play();
         videoPreview.muted = true;
+        recordBtn.innerHTML = "Stop recording";
+        streamObject = stream;
+        startRecording();
     } catch (error) {
         recordBtn.innerHTML = "Cannot record";
-        recordBtn.removeEventListener("click", startRecording);
+        recordBtn.removeEventListener("click", getVideo);
+    } finally {
+        recordBtn.removeEventListener("click", getVideo);
     }
 }
 
 function init() {
-    recordBtn.addEventListener("click", startRecording);
+    recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
