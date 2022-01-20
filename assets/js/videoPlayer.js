b4d766e204bc05230cf241ce6c1c973705a3c9a1
abhi-192 <contactabhi.2020@gmail.com>
2022-01-20 09:30:42 +0530

    video volume controls modify

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -7,12 +7,12 @@ const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 
 function handlePlayClick() {
-    if(videoPlayer.paused) {
+    if (videoPlayer.paused) {
         videoPlayer.play();
         playBtn.innerHTML = `<i class="fas fa-pause"></i>`;
         volumeBtn.innerHTML = `<i class="fas fa-volume-mute"></i>`;
     }
-    else{
+    else {
         videoPlayer.pause();
         playBtn.innerHTML = `<i class="fas fa-play"></i>`;
         volumeBtn.innerHTML = `<i class="fas fa-volume-mute"></i>`;
@@ -20,11 +20,11 @@ function handlePlayClick() {
 }
 
 function handleVolumeClick() {
-    if(videoPlayer.muted){
+    if (videoPlayer.muted) {
         videoPlayer.muted = false;
         volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
     }
-    else{
+    else {
         videoPlayer.muted = true;
         volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
     }
@@ -50,7 +50,7 @@ function exitFullScreen() {
 const formatDate = seconds => {
     const secondsNumber = parseInt(seconds, 10);
     let hours = Math.floor(secondsNumber / 3600);
-    let minutes = Math.floor((secondsNumber - hours * 3600)/ 60);
+    let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
     let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
 
     if (hours < 10) {
@@ -66,7 +66,7 @@ const formatDate = seconds => {
     return `${hours}:${minutes}:${totalSeconds}`;
 }
 
-function  goFullScreen() {
+function goFullScreen() {
     //implemented with vend or prefix webkit
     videoContainer.webkitRequestFullscreen();
     fullScrnBtn.innerHTML = `<i class="fas fa-compress"></i>`
@@ -75,13 +75,18 @@ function  goFullScreen() {
 }
 
 function getCurrentTime() {
-    currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+    currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
 }
 
-function setTotalTime(){
+function setTotalTime() {
     const totalTimeString = formatDate(videoPlayer.duration);
     totalTime.innerHTML = totalTimeString;
     setInterval(getCurrentTime, 1000);
+}
+
+function handleEnded() {
+    videoPlayer.currentTime = 0;
+    playBtn.innerHTML = `<i class="fas fa-play"></i>`;
 }
 
 function init() {
@@ -89,6 +94,7 @@ function init() {
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScrnBtn.addEventListener("click", goFullScreen);
     videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+    videoPlayer.addEventListener("ended", handleEnded);
 }
 
 videoContainer
